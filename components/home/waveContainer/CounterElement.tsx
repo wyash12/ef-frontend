5752ced7e3858148e658df7e57647338e4421413
Vyankatesh Nyati <vrnyati@gmail.com>
2023-07-12 19:27:12 +0530

    counter animation

--- components/home/waveContainer/CounterElement.tsx
+++ components/home/waveContainer/CounterElement.tsx
@@ -1,5 +1,8 @@
 import CountUp from "react-countup";
 import classes from "./CounterElement.module.scss";
+import { useInView } from "react-intersection-observer";
+import { useState } from "react";
+import { Bounce } from "react-awesome-reveal";
 
 interface propsType {
   icon: JSX.Element;
@@ -9,15 +12,35 @@ interface propsType {
 }
 
 export default function CounterElement(props: propsType) {
+  const { ref: counterRef, inView: counterInView } = useInView();
+  const [isInit, setIsInit] = useState(true);
+
   return (
-    <div className={classes.container}>
-      <div className={classes.subContainer}>
-        <h1>
-          {props.icon} <CountUp end={props.number} />+
-        </h1>
-      </div>
-      <p>{props.firstTitle}</p>
-      <p>{props.secondTitle}</p>
+    <div className={classes.container} ref={counterRef}>
+      <Bounce>
+        <div className={classes.subContainer}>
+          <h1>
+            {props.icon}{" "}
+            {counterInView && isInit ? (
+              <CountUp
+                end={props.number}
+                onEnd={() => {
+                  setIsInit(false);
+                }}
+                delay={0.1}
+                duration={2}
+              >
+                {({ countUpRef }) => <span ref={countUpRef} />}
+              </CountUp>
+            ) : (
+              <span>{props.number}</span>
+            )}
+            +
+          </h1>
+          <p>{props.firstTitle}</p>
+          <p>{props.secondTitle}</p>
+        </div>
+      </Bounce>
     </div>
   );
 }
