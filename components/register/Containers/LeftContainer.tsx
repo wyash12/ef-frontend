e39c3935b234f86c4c9bdf8777cd0f072cbcc65d
Vyankatesh Nyati <vrnyati@gmail.com>
2023-12-22 19:30:14 +0530

    Fix desktop view for register page

--- components/register/Containers/LeftContainer.tsx
+++ components/register/Containers/LeftContainer.tsx
@@ -5,13 +5,13 @@ import classes from "@/components/register/Containers/LeftCont.module.scss";
 
 export default function LeftContainer() {
   return (
-    <>
+    <div className={classes.container}>
       <Image src={reg1} alt="" className={classes.imgClass} />
 
       <p className={classes.text1}>
         Enhance your personal and professional skills to derive a hassle-free
         career growth!
       </p>
-    </>
+    </div>
   );
 }
--- components/register/Containers/RightContainer.tsx
+++ components/register/Containers/RightContainer.tsx
@@ -6,16 +6,17 @@ import Insert_Table from "@/public/Insert_Table.png";
 import classes from "@/components/register/Containers/RightContainer.module.scss";
 
 export default function RightContainer() {
-  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
-  const handleDateChange = (date: Date | null) => {
-    setSelectedDate(date);
-  };
+  // const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  // const handleDateChange = (date: Date | null) => {
+  //   setSelectedDate(date);
+  // };
 
   const handleSubmit = () => {
     console.log("submitted");
   };
+
   return (
-    <>
+    <div className={classes.container}>
       <h2 className={classes.heading}>Register</h2>
       <p className={classes.description}>
         Fill the form to join the internship Training program for civil
@@ -26,11 +27,12 @@ export default function RightContainer() {
           <label htmlFor="Name">
             Full Name<span className={classes.spanClass}>*</span>
           </label>
-          <div>
+          <div className={classes.inputWrapper}>
             <input
               type="text"
               className={classes.inputBox}
-              placeholder="Enter your full name"
+              // placeholder="Enter your full name"
+              required
             />
           </div>
         </div>
@@ -38,7 +40,7 @@ export default function RightContainer() {
           <label htmlFor="Email">
             Email<span className={classes.spanClass}>*</span>
           </label>
-          <div>
+          <div className={classes.inputWrapper}>
             <input type="text" className={classes.inputBox} />
           </div>
         </div>
@@ -47,39 +49,47 @@ export default function RightContainer() {
             College/Organization/Company Name
             <span className={classes.spanClass}>*</span>
           </label>
-          <div className="">
+          <div className={classes.inputWrapper}>
             <input type="text" className={classes.inputBox} />
           </div>
         </div>
         <div className={classes.bdayAndWhatsappDiv}>
-          <div className={classes.birthdayDiv}>
+          <div className={`${classes.birthdayDiv} ${classes.divClass}`}>
             <label htmlFor="Birtdate">
               Birthday
               <span className={classes.spanClass}>*</span>
             </label>
 
-            <input type="date" className={classes.inputBox} />
-            <Image src={Insert_Table} alt="" className={classes.imgClass} />
+            <div className={classes.inputWrapper}>
+              <input type="date" className={classes.inputBox} />
+            </div>
+            {/* <Image src={Insert_Table} alt="" className={classes.imgClass} /> */}
           </div>
-          <div className={classes.wappDiv}>
+          <div className={`${classes.wappDiv} ${classes.divClass}`}>
             <label htmlFor="Whatsapp Nubmer">
               WhatsApp No.<span className={classes.spanClass}>*</span>
             </label>
-            <input type="text" className={classes.inputBox} />
+            <div className={classes.inputWrapper}>
+              <input type="text" className={classes.inputBox} />
+            </div>
           </div>
         </div>
         <div className={classes.batchAndCourseDiv}>
-          <div className={classes.batchDiv}>
+          <div className={`${classes.batchDiv} ${classes.divClass}`}>
             <label htmlFor="Batch">
               Batch<span className={classes.spanClass}>*</span>
             </label>
-            <input type="text" className={classes.inputBox} />
+            <div className={classes.inputWrapper}>
+              <input type="text" className={classes.inputBox} />
+            </div>
           </div>
-          <div className={classes.courseDiv}>
+          <div className={`${classes.courseDiv} ${classes.divClass}`}>
             <label htmlFor="Course">
               Course<span className={classes.spanClass}>*</span>
             </label>
-            <input type="text" className={classes.inputBox} />
+            <div className={classes.inputWrapper}>
+              <input type="text" className={classes.inputBox} />
+            </div>
           </div>
         </div>
         <div className={classes.divClass}>
@@ -87,19 +97,19 @@ export default function RightContainer() {
             Current Address<span className={classes.spanClass}>*</span>
           </label>
           <div className="">
-            <input type="text" className={classes.inputBox} />
+            <div className={classes.inputWrapper}>
+              <input type="text" className={classes.inputBox} />
+            </div>
           </div>
         </div>
-        <div className={classes.registerDiv}>
-          <button
-            type="submit"
-            className={classes.register}
-            onSubmit={handleSubmit}
-          >
-            Register
-          </button>
-        </div>
+        <button
+          type="submit"
+          className={classes.register}
+          onSubmit={handleSubmit}
+        >
+          Register
+        </button>
       </form>
-    </>
+    </div>
   );
 }
